28fcbb84ba5ab7a0347af4a63caeb511161728d5
KimJunhoe <fish.gave.seaweed@gmail.com>
2024-12-18 16:42:14 +0900

    20241218
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-              <img src="/logo512.png" className="App-logo" alt="logo" />
+        <img src="/logo512.png" className="App-logo" alt="logo" />
         <p>
           <h2>Future in Beauty</h2>
-        안녕하세요, 주식회사 미래인 입니다!
+          안녕하세요, 주식회사 미래인 입니다!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          스마트 스토어로 이동
+          스토어로 이동
         </a>
       </header>
     </div>
